c5aaf8cd156bb3427491385c7dd187191cb6cc20
Xiwen Cheng <xiwen.cheng@mendix.com>
2016-01-29 23:40:54 +0100

    working example

--- hello.py
+++ hello.py
@@ -5,13 +5,16 @@ app = Flask(__name__)
 
 port = int(os.getenv("PORT", 9099))
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
 
+
 @app.route('/sample.css')
 def static_proxy():
-    return app.send_static_file('sample.css')
+    with open('sample.css') as f:
+        return f.read()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
